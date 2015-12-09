1f2c5b78354d781e72594645a930dbdcb90cd581
Joel McGrady <capttwinky@gmail.com>
2015-12-09 12:30:53 -0800

    Update echoscript.py

--- src/test/echoscript.py
+++ src/test/echoscript.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# jmcgrady@twitter.com
-#
-# echoscript.py
-# REFS: JIRA HWENG-
-# REPO: http://cgit.twitter.biz/tw_file_depot
 
 ## just a simple little testing script to process stdin -> stdout
 ## usefull for testing process protocols
